844c5cc600d94b378c366f7c5f403cdab6f8dabc
Aniket-Kote <65769296+Aniket-Kote@users.noreply.github.com>
2021-05-17 19:37:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ var lastUpdate=document.getElementById('lastUpdate');
 
 function show_data()
 {
-   var update=[];
+    var update=[];
     $.get('https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true',function(data){
        
         activecases.innerHTML=data['activeCases'];
@@ -27,6 +27,39 @@ function show_data()
              update.push(data['lastUpdatedAtApify'][i]);
         }
        lastUpdate.innerHTML=update.join("");
+
+
+
+       
+       for(var i=1;i<data['regionData'].length;i++)
+       {
+           var x=statewise_data.insertRow()
+
+           x.insertCell(0)
+           statewise_data.rows[i].cells[0].innerHTML=i
+           statewise_data.rows[i].cells[0].style.backgroundColor="#fff";
+           statewise_data.rows[i].cells[0].style.textAlign='center';
+
+           x.insertCell(1)
+           statewise_data.rows[i].cells[1].innerHTML=data['regionData'][i-1]['region']
+           statewise_data.rows[i].cells[1].style.backgroundColor="#4bb7d8";
+           statewise_data.rows[i].cells[1].style.fontWeight='bold';
+
+           x.insertCell(2)
+           statewise_data.rows[i].cells[2].innerHTML=data['regionData'][i-1]['totalInfected']
+           statewise_data.rows[i].cells[2].style.backgroundColor="#f36e23"
+
+           x.insertCell(3)
+           statewise_data.rows[i].cells[3].innerHTML=data['regionData'][i-1]['recovered']
+           statewise_data.rows[i].cells[3].style.backgroundColor="#4bb7d8"
+
+           x.insertCell(4)
+           statewise_data.rows[i].cells[4].innerHTML=data['regionData'][i-1]['deceased']
+           statewise_data.rows[i].cells[4].style.backgroundColor="#f36e23"
+       }
+
+
+
 
     })
 }
